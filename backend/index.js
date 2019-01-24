0d7028dc193320dfaf2f5950aac7aa8baa38cda5
geowat <ldsxxs@163.com>
2019-01-24 17:23:44 +0800

    edit page and mobile index.js

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,9 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '/desktop/dist')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/desktop/dist/index.html')))
-app.get('/m', (req, res) => res.sendFile(path.join(__dirname, '/mobile/dist/index.html')))
+// app.get('/m', (req, res) => res.sendFile(path.join(__dirname, '/mobile/dist/index.html')))
 
 app.listen(8899, () => console.log('fund_app is running at port 8899.'))
