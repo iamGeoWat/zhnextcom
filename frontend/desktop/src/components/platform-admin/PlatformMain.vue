5a4356bb9da1475975ed4faaedc1ec23dcff4630
QiYuan <760752580@qq.com>
2019-02-20 17:07:03 +0800

    完成

--- frontend/desktop/src/components/platform-admin/PlatformMain.vue
+++ frontend/desktop/src/components/platform-admin/PlatformMain.vue
@@ -84,7 +84,133 @@
           <div id="pieChart" :style="{width: '95%', height: '80%'}"></div>
         </div>
       </div>
-      <p class="title" style="margin-top: 10px">资金子账号</p>
+      <p class="title" style="margin-top: 30px">资金子账号</p>
+      <div class="bottom-data">
+        <div class="bottom-item">
+          <div class="item-top">
+            <p>法币账户-A</p>
+            <el-button class="myButton" size="mini" round @click="download()">导出表格</el-button>
+          </div>
+          <div class="item-content">
+            <div class="top-item">
+              <p class="fontFamily">总资产(BTC)</p>
+              <p class="data">108.656</p>
+            </div>
+            <div class="top-item">
+              <p class="fontFamily">运行天数</p>
+              <p class="data">11</p>
+            </div>
+            <div class="top-item">
+              <p class="fontFamily">净值</p>
+              <p class="data">1.33</p>
+            </div>
+          </div>
+          <div class="item-content2">
+            <div class="top-item">
+              <p class="fontFamily">目前收益</p>
+              <p class="data">33.62</p>
+            </div>
+            <div class="top-item" style="margin-left: 25%">
+              <p class="fontFamily">动态年化</p>
+              <p class="data">528.15</p>
+            </div>
+          </div>
+          <div id="item-lineChart" :style="{width: '100%', height: '60%'}"></div>
+        </div>
+        <div class="bottom-item">
+          <div class="item-top">
+            <p>法币账户-A</p>
+            <el-button class="myButton" size="mini" round @click="download()">导出表格</el-button>
+          </div>
+          <div class="item-content">
+            <div class="top-item">
+              <p class="fontFamily">总资产(BTC)</p>
+              <p class="data">108.656</p>
+            </div>
+            <div class="top-item">
+              <p class="fontFamily">运行天数</p>
+              <p class="data">11</p>
+            </div>
+            <div class="top-item">
+              <p class="fontFamily">净值</p>
+              <p class="data">1.33</p>
+            </div>
+          </div>
+          <div class="item-content2">
+            <div class="top-item">
+              <p class="fontFamily">目前收益</p>
+              <p class="data">33.62</p>
+            </div>
+            <div class="top-item" style="margin-left: 25%">
+              <p class="fontFamily">动态年化</p>
+              <p class="data">528.15</p>
+            </div>
+          </div>
+          <div id="item-lineChart2" :style="{width: '100%', height: '60%'}"></div>
+        </div>
+        <div class="bottom-item">
+          <div class="item-top">
+            <p>法币账户-A</p>
+            <el-button class="myButton" size="mini" round @click="download()">导出表格</el-button>
+          </div>
+          <div class="item-content">
+            <div class="top-item">
+              <p class="fontFamily">总资产(BTC)</p>
+              <p class="data">108.656</p>
+            </div>
+            <div class="top-item">
+              <p class="fontFamily">运行天数</p>
+              <p class="data">11</p>
+            </div>
+            <div class="top-item">
+              <p class="fontFamily">净值</p>
+              <p class="data">1.33</p>
+            </div>
+          </div>
+          <div class="item-content2">
+            <div class="top-item">
+              <p class="fontFamily">目前收益</p>
+              <p class="data">33.62</p>
+            </div>
+            <div class="top-item" style="margin-left: 25%">
+              <p class="fontFamily">动态年化</p>
+              <p class="data">528.15</p>
+            </div>
+          </div>
+          <div id="item-lineChart3" :style="{width: '100%', height: '60%'}"></div>
+        </div>
+        <div class="bottom-item">
+          <div class="item-top">
+            <p>法币账户-A</p>
+            <el-button class="myButton" size="mini" round @click="download()">导出表格</el-button>
+          </div>
+          <div class="item-content">
+            <div class="top-item">
+              <p class="fontFamily">总资产(BTC)</p>
+              <p class="data">108.656</p>
+            </div>
+            <div class="top-item">
+              <p class="fontFamily">运行天数</p>
+              <p class="data">11</p>
+            </div>
+            <div class="top-item">
+              <p class="fontFamily">净值</p>
+              <p class="data">1.33</p>
+            </div>
+          </div>
+          <div class="item-content2">
+            <div class="top-item">
+              <p class="fontFamily">目前收益</p>
+              <p class="data">33.62</p>
+            </div>
+            <div class="top-item" style="margin-left: 25%">
+              <p class="fontFamily">动态年化</p>
+              <p class="data">528.15</p>
+            </div>
+          </div>
+          <div id="item-lineChart4" :style="{width: '100%', height: '60%'}"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -114,7 +240,6 @@
       if (sessionStorage.getItem("userId") === null) {
         this.$router.push({path: '/'})
       }
-      // this.drawLine();
       this.getDot("6h");
       this.drawPie();
     },
@@ -192,6 +317,298 @@
               shadowOffsetY: 2
             }
           }],
+        });
+        window.addEventListener("resize", () => {
+          this.lineChart.resize();
+        });
+      },
+      drawItemLine() {
+        this.lineChart = this.$echarts.init(document.getElementById('item-lineChart'));
+        this.lineChart.setOption({
+          tooltip: {
+            trigger: 'axis',
+            position: function (pt) {
+              return [pt[0], '10%'];
+            }
+          },
+          xAxis: {
+            type: 'category',
+            data: this.lineChartDate
+          },
+          yAxis: {
+            type: 'value',
+          },
+          legend: {
+            x: 'left',
+            bottom: 0,
+          },
+          series: [
+            {
+              name:"币本位收益",
+              data: this.lineChartData,
+              type: 'line',
+              symbol: 'circle',
+              itemStyle: {
+                normal: {
+                  color: "rgb(112, 152, 242)", //折线点的颜色
+                  lineStyle: {
+                    color: "rgb(20, 99, 162)" //折线的颜色
+                  }
+                }
+              },
+            },
+            {
+              name:"法币本位",
+              data: [3,7,8,10,14,18,16,19,22,23,25],
+              type: 'line',
+              symbol: 'circle',
+              itemStyle: {
+                normal: {
+                  color: "rgb(225, 210, 120)", //折线点的颜色
+                  lineStyle: {
+                    color: "rgb(219, 197, 111)" //折线的颜色
+                  }
+                }
+              },
+            }
+          ],
+          dataZoom: [
+            {
+              type: 'inside',
+              start: 0,
+              end: 50
+            }, {
+              start: 0,
+              end: 50,
+              handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+              handleSize: '80%',
+              handleStyle: {
+                color: '#fff',
+                shadowBlur: 3,
+                shadowColor: 'rgba(0, 0, 0, 0.6)',
+                shadowOffsetX: 2,
+                shadowOffsetY: 2
+              }
+            }],
+        });
+        window.addEventListener("resize", () => {
+          this.lineChart.resize();
+        });
+      },
+      drawItemLine2() {
+        this.lineChart = this.$echarts.init(document.getElementById('item-lineChart2'));
+        this.lineChart.setOption({
+          tooltip: {
+            trigger: 'axis',
+            position: function (pt) {
+              return [pt[0], '10%'];
+            }
+          },
+          xAxis: {
+            type: 'category',
+            data: this.lineChartDate
+          },
+          yAxis: {
+            type: 'value',
+          },
+          legend: {
+            x: 'left',
+            bottom: 0,
+          },
+          series: [
+            {
+              name:"币本位收益",
+              data: this.lineChartData,
+              type: 'line',
+              symbol: 'circle',
+              itemStyle: {
+                normal: {
+                  color: "rgb(112, 152, 242)", //折线点的颜色
+                  lineStyle: {
+                    color: "rgb(20, 99, 162)" //折线的颜色
+                  }
+                }
+              },
+            },
+            {
+              name:"法币本位",
+              data: [3,7,8,10,14,18,16,19,22,23,25],
+              type: 'line',
+              symbol: 'circle',
+              itemStyle: {
+                normal: {
+                  color: "rgb(225, 210, 120)", //折线点的颜色
+                  lineStyle: {
+                    color: "rgb(219, 197, 111)" //折线的颜色
+                  }
+                }
+              },
+            }
+          ],
+          dataZoom: [
+            {
+              type: 'inside',
+              start: 0,
+              end: 50
+            }, {
+              start: 0,
+              end: 50,
+              handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+              handleSize: '80%',
+              handleStyle: {
+                color: '#fff',
+                shadowBlur: 3,
+                shadowColor: 'rgba(0, 0, 0, 0.6)',
+                shadowOffsetX: 2,
+                shadowOffsetY: 2
+              }
+            }],
+        });
+        window.addEventListener("resize", () => {
+          this.lineChart.resize();
+        });
+      },
+      drawItemLine3() {
+        this.lineChart = this.$echarts.init(document.getElementById('item-lineChart3'));
+        this.lineChart.setOption({
+          tooltip: {
+            trigger: 'axis',
+            position: function (pt) {
+              return [pt[0], '10%'];
+            }
+          },
+          xAxis: {
+            type: 'category',
+            data: this.lineChartDate
+          },
+          yAxis: {
+            type: 'value',
+          },
+          legend: {
+            x: 'left',
+            bottom: 0,
+          },
+          series: [
+            {
+              name:"币本位收益",
+              data: this.lineChartData,
+              type: 'line',
+              symbol: 'circle',
+              itemStyle: {
+                normal: {
+                  color: "rgb(112, 152, 242)", //折线点的颜色
+                  lineStyle: {
+                    color: "rgb(20, 99, 162)" //折线的颜色
+                  }
+                }
+              },
+            },
+            {
+              name:"法币本位",
+              data: [3,7,8,10,14,18,16,19,22,23,25],
+              type: 'line',
+              symbol: 'circle',
+              itemStyle: {
+                normal: {
+                  color: "rgb(225, 210, 120)", //折线点的颜色
+                  lineStyle: {
+                    color: "rgb(219, 197, 111)" //折线的颜色
+                  }
+                }
+              },
+            }
+          ],
+          dataZoom: [
+            {
+              type: 'inside',
+              start: 0,
+              end: 50
+            }, {
+              start: 0,
+              end: 50,
+              handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+              handleSize: '80%',
+              handleStyle: {
+                color: '#fff',
+                shadowBlur: 3,
+                shadowColor: 'rgba(0, 0, 0, 0.6)',
+                shadowOffsetX: 2,
+                shadowOffsetY: 2
+              }
+            }],
+        });
+        window.addEventListener("resize", () => {
+          this.lineChart.resize();
+        });
+      },
+      drawItemLine4() {
+        this.lineChart = this.$echarts.init(document.getElementById('item-lineChart4'));
+        this.lineChart.setOption({
+          tooltip: {
+            trigger: 'axis',
+            position: function (pt) {
+              return [pt[0], '10%'];
+            }
+          },
+          xAxis: {
+            type: 'category',
+            data: this.lineChartDate
+          },
+          yAxis: {
+            type: 'value',
+          },
+          legend: {
+            x: 'left',
+            bottom: 0,
+          },
+          series: [
+            {
+              name:"币本位收益",
+              data: this.lineChartData,
+              type: 'line',
+              symbol: 'circle',
+              itemStyle: {
+                normal: {
+                  color: "rgb(112, 152, 242)", //折线点的颜色
+                  lineStyle: {
+                    color: "rgb(20, 99, 162)" //折线的颜色
+                  }
+                }
+              },
+            },
+            {
+              name:"法币本位",
+              data: [3,7,8,10,14,18,16,19,22,23,25],
+              type: 'line',
+              symbol: 'circle',
+              itemStyle: {
+                normal: {
+                  color: "rgb(225, 210, 120)", //折线点的颜色
+                  lineStyle: {
+                    color: "rgb(219, 197, 111)" //折线的颜色
+                  }
+                }
+              },
+            }
+          ],
+          dataZoom: [
+            {
+              type: 'inside',
+              start: 0,
+              end: 50
+            }, {
+              start: 0,
+              end: 50,
+              handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+              handleSize: '80%',
+              handleStyle: {
+                color: '#fff',
+                shadowBlur: 3,
+                shadowColor: 'rgba(0, 0, 0, 0.6)',
+                shadowOffsetX: 2,
+                shadowOffsetY: 2
+              }
+            }],
         });
         window.addEventListener("resize", () => {
           this.lineChart.resize();
@@ -324,6 +741,10 @@
           this.lineChartData = response.data[0];
           console.log(this.lineChartData, this.lineChartDate);
           this.drawLine();
+          this.drawItemLine();
+          this.drawItemLine2();
+          this.drawItemLine3();
+          this.drawItemLine4();
         });
       }
     },
@@ -446,6 +867,66 @@
           #pieChart {
             margin-left: 30px;
             margin-top: 20px;
+          }
+        }
+      }
+      .bottom-data{
+        display: flex;
+        justify-content: space-between;
+        height: 600px;
+        width: 100%;
+        min-width: 1300px;
+        margin-top: 5px;
+        .bottom-item{
+          display: inline-block;
+          height: 600px;
+          width: 24%;
+          background:#fff;
+          border-radius: 10px;
+          .item-top{
+            width: 90%;
+            margin: 15px auto;
+            p{
+              display: inline-block;
+              font-size: 18px;
+            }
+            .myButton{
+              background-color: rgb(240,240,240);
+              color: #000;
+              font-size: 18px;
+              float: right;
+            }
+          }
+          .item-content{
+            display: flex;
+            justify-content: space-between;
+            width: 90%;
+            margin: 0 auto;
+            .top-item{
+              text-align: center;
+              .fontFamily{
+                font-family: font1;
+                font-size: 18px;
+              }
+              .data{
+                font-size: 28px;
+              }
+            }
+          }
+          .item-content2{
+            width: 90%;
+            margin: 15px auto;
+            .top-item{
+              display: inline-block;
+              text-align: center;
+              .fontFamily{
+                font-family: font1;
+                font-size: 18px;
+              }
+              .data{
+                font-size: 28px;
+              }
+            }
           }
         }
       }
